32c1d5a7ee45622f4217723e44600ddfbbe84bfb
Martin Matijevic <martinmatijevic@hotmail.com>
2020-08-24 12:58:47 +0200

    update

---
+++ proba/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int x;
+    if (!(cin >> x)){
+        cout << "los unos" << endl;
+    }
+    cout << "X = " << x << endl;
+    return 0;
+}
